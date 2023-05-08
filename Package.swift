d00b1c34320640fe14a041c31d7658fb5b364acd
Arror <hallo.maqiang@icloud.com>
2023-05-08 01:51:52 +0000

    Tun2SocksKit Release 2.4.0!!!

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     ),
     .binaryTarget(
         name: "HevSocks5Tunnel",
-        url: "https://github.com/arror/Tun2SocksKit/releases/download/2.3.0/HevSocks5Tunnel.xcframework.zip",
-        checksum: "c98f1f0f7f3073724db345373ea588b5f461ade0d3bb45768fbef490d9ffe46d"
+        url: "https://github.com/arror/Tun2SocksKit/releases/download/2.4.0/HevSocks5Tunnel.xcframework.zip",
+        checksum: "3f0f72711b02b0f1ed363c1b5760ff0f94e9b13851f5f95658b63314e6517e5d"
     )
   ]
 )
